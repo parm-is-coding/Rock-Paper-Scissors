1e8f3f0ac672dc3be13722840f73f7e475a54384
parm-is-coding <pardhi1400@gmail.com>
2023-07-02 15:16:34 -0700

    create game function

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function getComputerChoice(){
 }
 function getPlayerChoice(){
     let result = prompt("Enter Rock Paper or Scissors");
-    return lower(result);
+    return result.toLowerCase();
 }
 function rPSRound(computerChoice,playerChoice){
 if(computerChoice === playerChoice){
@@ -34,8 +34,25 @@ if(computerChoice === playerChoice){
             }
 }
 
-console.log((rPSRound(getComputerChoice(),getPlayerChoice())));
 
 
+function game(){
+    playerWins = 0;
+    compWins = 0;
+    for( i = 0; i < 5; i++){
+        result = rPSRound(getComputerChoice(),getPlayerChoice());
+        if(result.substring(0,7) == "You Lose"){
+            compWins++;
+        }else{
+            playerWins++;
+        }
+    }
+    if(playerWins > compWins){
+        console.log(`You Win ${playerWins} times`);
+    }else {
+        console.log(`You Lose ${compWins}`);
+    }
+}
 
+console.log(game());
 
