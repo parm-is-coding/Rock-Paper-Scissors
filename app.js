066af980eab1506eb3af0565f7267b2ef3a7064d
parm-is-coding <pardhi1400@gmail.com>
2023-07-02 14:55:00 -0700

    fix bug in rPS function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//randomly return Rock Paper or Scissors
 function getComputerChoice(){
     let rndNum = Math.floor(Math.random()*3);
     switch(rndNum){
@@ -13,10 +14,27 @@ function getComputerChoice(){
     }
 
 }
-
 function getPlayerChoice(){
-    prompt("")
+    return prompt("Enter Rock Paper or Scissors");
 }
-console.log(getComputerChoice());
+function rPSRound(computerChoice,playerChoice){
+if(computerChoice === playerChoice){
+    return "Tie no winner";
+}else if(playerChoice == "Rock" &&
+         computerChoice == "Paper" ||
+         playerChoice == "Scissors" &&
+         computerChoice == "Rock" ||
+         playerChoice == "Paper" && 
+         computerChoice == "Rock"
+            ){
+                return `You Lose Computer chose ${computerChoice} and you chose ${playerChoice}`;
+            }else{
+                return `You Win! Computer chose ${computerChoice} and you chose ${playerChoice}`;
+            }
+}
+
+console.log((rPSRound(getComputerChoice(),getPlayerChoice())));
+
+
 
 
